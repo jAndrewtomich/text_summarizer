efb8612ed2a5a097451278bacd0df3bc17c5aff9
jatomic <johnatomich@gmail.com>
2021-12-08 16:37:13 -0600

    Refactor and separate concerns

--- main.py
+++ main.py
@@ -1,71 +1,41 @@
-import funcs # import our functions
-import os # filesystem and environment variables
-import datetime # system time for record keeping
+from dotenv import load_dotenv # load environment variables
 
-import smtplib # sending and formatting email    - | 
-from email.mime.multipart import MIMEMultipart # - |
-from email.mime.text import MIMEText #  -  -  -  - |
+from os.path import dirname, join # get path to .env file
 
-from dotenv import load_dotenv # load environment variables  - |
-load_dotenv() #   -   -   -   -   -   -   -   -   -   -   -  - |
+from headline_manager import HeadlineManager
+from email_manager import EmailManager
+from news_manager import NewsManager
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
 
 def main():
-    now = datetime.datetime.now()
-    content = ""
+    headline_m = HeadlineManager()
+    headlines = headline_m.extract_headlines()
 
-    cnt = funcs.extract_news(input("Enter URL: ") or os.environ["DEFAULT_NEWS_URL"])
-    content += cnt
-    content += ("<br>------<br>")
-    content += ("<br><br>End of Message")
-
-    """
-    send the email
-    """
-    msg = MIMEMultipart()
-    msg["Subject"] = f"Top News Stories [Automated Email] {str(now.month)}-{str(now.day)}-{str(now.year)}"
-    msg["From"] = os.environ["FROM"]
-    msg["To"] = os.environ["TO"]
-
-    msg.attach(MIMEText(content, "html"))
-
-    print("Initializing Server...")
-
-    server = smtplib.SMTP(os.environ["MAIL_SERVER"], os.environ["MAIL_PORT"])
-    server.set_debuglevel(1)
-    server.ehlo()
-    server.starttls()
-    server.login(os.environ["FROM"], os.environ["MAIL_PW"])
-    server.sendmail(os.environ["FROM"], os.environ["TO"], msg.as_string())
-
-    print("Email Sent...")
-
-    server.quit()
-
-    """
-    get text from target url
-    """
-    text = funcs.get_only_text( \
-        input("Enter URL: ") or \
-        os.environ["DEFAULT_TARGET_URL"]) # use default url from env variables if no input
+    email_m = EmailManager()
+    #email_m.send_email(headlines)
+    
+    news_m = NewsManager(url="https://www.voxmedia.com/2021/11/19/22791332/the-second-season-of-vox-and-vox-media-studios-the-mind-explained-premieres-today-on-netflix")
 
     """
     display link text and text length
     """ 
-    print(len(text)) # length of tuple
-    print(len(str.split(text[1]))) # number of words in link text, aside from title
-    print(text) # full text of link
+    print(len(news_m.text)) # length of string
+    print(len(str.split(news_m.text))) # number of words in link text, aside from title
+    print(news_m.text) # full text of link
 
     """
     summarize text
     """
-    txt_summ = funcs.summ(text) # call summarizer
+    txt_summ = news_m.summ() # call summarizer
     print(txt_summ) # view output
 
     """
     produce keywords from summary
     """
-    k_words = funcs.get_keyword_lemmas(text) # get lemmatized keywords
+    k_words = news_m.get_keyword_lemmas() # get lemmatized keywords
     print(k_words) # view output
 
 
