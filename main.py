0c9c112c4a23a5b945947963744156f4de0be04d
jatomic <johnatomich@gmail.com>
2021-12-14 03:12:41 -0600

    All headlines are summarized. Output to file.

--- main.py
+++ main.py
@@ -9,34 +9,46 @@ from news_manager import NewsManager
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
+import sys
+import os
+
 
 def main():
-    headline_m = HeadlineManager()
-    headlines = headline_m.extract_headlines()
+
+    if len((args := sys.argv)) > 1:
+        if os.path.isfile(args[1]):
+            with open(args[1], 'r') as reader:
+                news_sites = reader.readlines()
+    else:
+        news_sites = [None]
+
+    for site in news_sites:
+        headline_m = HeadlineManager(site.strip())
+        headlines, hlList = headline_m.extract_headlines()
+
+
+        for hl in hlList: 
+            news_m = NewsManager(hl) if hl else NewsManager()
+
+            """
+            summarize text
+            """
+            txt_summ = news_m.summ() # call summarizer
+            with open("output.txt", 'a') as writer:
+                writer.write(txt_summ + "\n")
+
+            """
+            produce keywords from summary
+            """
+            k_words = news_m.get_keyword_lemmas() # get lemmatized keywords
+            with open("output.txt", 'a') as writer:
+                writer.write(k_words + "**")
+
+    with open("output.txt", 'r') as reader:
+        full_text = reader.read()
 
     email_m = EmailManager()
-    #email_m.send_email(headlines)
-    
-    news_m = NewsManager()
-
-    """
-    display link text and text length
-    """ 
-    print(len(news_m.text)) # length of string
-    print(len(str.split(news_m.text))) # number of words in link text, aside from title
-    print(news_m.text) # full text of link
-
-    """
-    summarize text
-    """
-    txt_summ = news_m.summ() # call summarizer
-    print(txt_summ) # view output
-
-    """
-    produce keywords from summary
-    """
-    k_words = news_m.get_keyword_lemmas() # get lemmatized keywords
-    print(k_words) # view output
+    email_m.send_email(full_text)
 
 
 if __name__ == "__main__":
