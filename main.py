afccc8f701361f3110ab34d10f95cf144127bcca
jatomic <johnatomich@gmail.com>
2021-12-14 12:37:01 -0600

    Streamline output file generation and removed redundant manager class

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import sys # argv
 import os # check validity of filenames, delete output file
 import validators # check urls
 
-from headline_manager import HeadlineManager # Each valid url (either from input file or command line args) will be scraped for headline links
-from email_manager import EmailManager # Create and send emails
+#from headline_manager import HeadlineManager # Each valid url (either from input file or command line args) will be scraped for headline links
+#from email_manager import EmailManager # Create and send emails
 from news_manager import NewsManager # Perform text summarization and extract keywords from each article
 
 dotenv_path = join(dirname(__file__), ".env")
@@ -25,43 +25,18 @@ def main():
     else:
         news_sites = [None] # default value
 
-    if os.path.isfile("output.txt"): os.remove("output.txt") # remove any old copy of output to avoid duplicate information
+    if os.path.isdir("output"): 
+        for f in os.listdir("output"):
+            os.remove(f"output/{f}")
+        os.rmdir("output") # remove any old copy of output to avoid duplicate information
 
-    for site in news_sites:
+    os.mkdir("output")
 
-        headline_m = HeadlineManager() if not site else HeadlineManager(site.strip())
+    for i, site in enumerate(news_sites):
+        news_m = NewsManager(i) if not site else NewsManager(i, site.strip())
 
-        for hl in headline_m.hlList:
-            news_m = NewsManager(hl)
-
-            """
-            summarize text
-            """
-            txt_summ = news_m.summ() # call summarizer
-            with open("output.txt", 'a') as writer: # write to temporary output file so results can be more easily concatenated
-                writer.write(txt_summ + "\n" + "<br>")
-
-            """
-            produce keywords from summary
-            """
-            k_words = news_m.get_keyword_lemmas() # get lemmatized keywords
-            with open("output.txt", 'a') as writer:
-                writer.write(k_words + "\n" + "<br>------<br>")
-
-            del news_m # cleanup memory
-        del headline_m # cleanup memory
-
-    with open("output.txt", 'r') as reader: # concatenate output in order to send in single email
-        full_text = reader.read()
-
-    email_m = EmailManager() # create email from total output
-    email_m.send_email(full_text)
-
-    os.remove("output.txt") # delete output file
+        print(news_m.generate_summaries())
 
 
 if __name__ == "__main__":
     main()
-
-
-
