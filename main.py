ecab56d53f085f147ea836b38d39c11e34c7da2d
jatomic <johnatomich@gmail.com>
2021-12-14 05:17:41 -0600

    Reformat email using html

--- main.py
+++ main.py
@@ -10,53 +10,53 @@ from headline_manager import HeadlineManager # Each valid url (either from input
 from email_manager import EmailManager # Create and send emails
 from news_manager import NewsManager # Perform text summarization and extract keywords from each article
 
-
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 
 def main():
 
+    news_sites = [None]
+
     if len((args := sys.argv)) > 1:
         if os.path.isfile(args[1]):
             with open(args[1], 'r') as reader:
-                news_sites = reader.readlines()
+                news_sites = str.split(reader.read())
         elif validators.url(args[1]):
+            print("valid")
             news_sites = [args[1]]
         else:
             print("Error: Invalid arguments")
-    else:
-        news_sites = [None]
+            exit(1)
 
     for site in news_sites:
-        headline_m = HeadlineManager(site.strip())
-        headlines, hlList = headline_m.extract_headlines()
+        headline_m = HeadlineManager() if not site else HeadlineManager(site.strip())
+        hlList = headline_m.extract_headlines()
 
-        for hl in hlList: 
+        for hl in hlList:
             news_m = NewsManager(hl) if hl else NewsManager()
 
             """
             summarize text
             """
             txt_summ = news_m.summ() # call summarizer
-            with open("output.txt", 'a') as writer:
-                writer.write(txt_summ)
+            with open("output.txt", 'a') as writer: # write to temporary output file so results can be more easily concatenated
+                writer.write(txt_summ + "\n" + "<br>")
 
             """
             produce keywords from summary
             """
             k_words = news_m.get_keyword_lemmas() # get lemmatized keywords
             with open("output.txt", 'a') as writer:
-                writer.write(k_words)
+                writer.write(k_words + "\n" + "<br>------<br>")
 
-    with open("output.txt", 'r') as reader:
+    with open("output.txt", 'r') as reader: # concatenate output in order to send in single email
         full_text = reader.read()
 
-    print(headlines)
-    print(full_text)
-    #email_m = EmailManager()
-    #email_m.send_email(full_text)
-    os.remove("output.txt")
+    email_m = EmailManager() # create email from total output
+    email_m.send_email(full_text)
+
+    os.remove("output.txt") # delete output file
 
 
 if __name__ == "__main__":
